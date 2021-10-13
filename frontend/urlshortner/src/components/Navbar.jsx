ac388f3e12bef9e4c62088d8008467ea8f81dc5f
infinitumkiran <gobenkabhi@gmail.com>
2021-10-13 19:46:27 +0530

    added button,herosection and navbar

---
+++ frontend/urlshortner/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import {AppBar,Toolbar,Typography,IconButton,makeStyles} from '@mui/material';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+<AppBar position="absolute" style={{backgroundColor: "black",height: "10%"}}>
+  <Toolbar variant="dense">
+    <NavLink to ="./" style= { {color:"cyan",
+        textDecoration: 'none',
+        marginTop: "18px",
+        marginLeft: "200px",
+        fontSize: 35,
+        fontFamily:'arial'
+    }}>
+      INFINITLY  
+    </NavLink>
+    
+  </Toolbar>
+  
+</AppBar>
+    )
+}
+
+export default NavBar;
